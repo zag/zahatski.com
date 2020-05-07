87aa7416aa0deb14bb80dc3372f22cbf8fa4f977
Aliaksandr Zahatski <github.send@zag.ru>
2020-05-07 18:58:53 +0300

    the first commit

---
+++ config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  url: 'https://zahatski.com',
+  pathPrefix: '/',
+  title: 'Alexandr Zahatski| software engeneering, electronic publishing',
+  subtitle: 'Alexandr Zahatski| software engeneering, electronic publishing',
+  copyright: '© All rights reserved. 2002-2020.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-2388080-5',
+  useKatex: false,
+  menu: [
+    {
+      label: 'About',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    },
+],
+  author: {
+    name: 'Alexandr Zahatski',
+    photo: '/photo.jpg',
+    bio: `I am interested in personal publishing, decentralized systems and software engineering 💻.`,
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'zag',
+      rss: 'https://zahatski.com/rss.xml',
+      vkontakte: '',
+      linkedin: 'zahatski',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: ''
+    }
+  }
+};
